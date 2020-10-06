867af45eae2357c5f7a741d8f8af36d82dfde68c
AustinKavelman <kavea9410@wrdsb.ca>
2020-10-06 14:29:23 +0000

    finished the factorial value calculator.

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * This program calculates the intergers factorial value
+ * @author Austin Kavelman
  */
 public class Main {
 
@@ -9,7 +11,23 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //Variables for number, the factorial, and accumulator variable
+    int i, num, fac = 1;
+
+    //asks the user to enter the interger to calculate
+    System.out.println("Please enter an integer to calculate the factorial of");
+    //allows user to enter the interger to calculate
+    num = input.nextInt();
+  
+    //calculates the interger
+    for(i = 1; i <= num; i++){
+      fac = fac * i;
+    }
+    System.out.println(num + "! = " + fac);
+
     
   }
 }
